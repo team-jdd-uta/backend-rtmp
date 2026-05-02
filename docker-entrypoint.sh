e0dbc32cfcdb823915c681cd90a22ef921ff4a74
kimyoobin <55120730+KimYooBin1@users.noreply.github.com>
2026-05-02 22:43:21 +0900

    Fix RTMP callback DNS

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 : "${RTMP_CALLBACK_SECRET:=rtmp-dev-secret}"
+export RTMP_CALLBACK_SECRET
 envsubst '${RTMP_CALLBACK_SECRET}' \
   < /etc/nginx/nginx.conf.template \
   > /etc/nginx/nginx.conf
